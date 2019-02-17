e6c54794eb7886627b6eaa79beecdc839d3f7672
Alex Shy <ShyninLX@gmail.com>
2019-02-17 18:13:45 +0200

    updates for API project

--- ApiProject/themovie/routes/index.js
+++ ApiProject/themovie/routes/index.js
@@ -5,7 +5,7 @@ var api_key='f981cff44272509c2bd87ca71cdc774f';
 /* GET home page. */
 router.get('/:pagination', function(req, res, next) {
   var pagination = req.params.pagination;
-  request('https://api.themoviedb.org/3/discover/movie?api_key='+api_key+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', function (error, response, body) {
+  request('https://api.themoviedb.org/3/discover/movie?api_key='+api_key+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&'+pagination+'', function (error, response, body) {
   // request('https://api.themoviedb.org/3/discover/movie?api_key=f981cff44272509c2bd87ca71cdc774f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var moviesArray = JSON.parse(body).results;
